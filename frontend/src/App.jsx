8f2afea99879178f9a117279c168e0153aef9a47
chaudharysagar <sagarchaudharynn06@gmail.com>
2024-09-07 10:59:06 +0530

    header and explore menu added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/Cart/Cart'
+import Placeorder from './pages/PlaceOrder/Placeorder'
+import Home from './pages/Home/Home'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<Placeorder />} />
+        <Route />
+      </Routes>
     </div>
   )
 }
