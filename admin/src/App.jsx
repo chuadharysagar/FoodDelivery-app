0a4d8892cccff538417fa512719f7ad226ca0fa6
chaudharysagar <sagarchaudharynn06@gmail.com>
2024-09-08 16:22:06 +0530

    added add section in admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+import { Routes, Route } from 'react-router-dom'
+import Add from './components/pages/Add/Add'
+import List from './components/pages/List/List'
+import Orders from './components/pages/Orders/Orders'
+
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+   return (
+      <div>
+         <Navbar />
+         <hr />
+         <div className="app-content">
+            <Sidebar />
+            <Routes>
+              <Route path='/add' element={<Add/>} />
+              <Route path='/list' element={<List/>} />
+              <Route path='/orders' element={<Orders/>} />
+            </Routes>
+         </div>
+      </div>
+   )
 }
 
 export default App
