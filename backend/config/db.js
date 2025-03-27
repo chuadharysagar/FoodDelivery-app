520c0359cb16e2d5a984ea37270708efd2d951a3
chaudharysagar <sagarchaudharynn06@gmail.com>
2025-03-27 22:21:14 +0530

    env file

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 
 export const connectDB = async () => {
-   await mongoose.connect("mongodb://localhost:27017/foodapp").then(() => { console.log("DB connected") }
+   await mongoose.connect(process.env.MONGODB_API_KEY).then(() => { console.log("DB connected") }
    )
 }
